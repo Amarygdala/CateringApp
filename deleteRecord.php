825e03385b1b6e466b06e36bd208662b81ef8a74
Amarygdala <49155656+Amarygdala@users.noreply.github.com>
2019-05-13 10:54:29 -0400

    Added comments for some functionality
    Usage of burger icon and it's JS script function. Table creation. Input boxes. User deletion by ID. Warnings.

--- deleteRecord.php
+++ deleteRecord.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="styletable.css">
 </header>
 <body>
-
+<!--Burger icon that shows a dropdown to redirect to other pages-->
 <div class="burgerTOP">
 <div class="burger">
     <button onclick="dropFunction()" class="dropbtn"></button>
@@ -20,10 +20,11 @@
 <div class="burger"></div>
 <div class="burger"></div>
 </div>
-
+<!--JS function that show/hide the dropdown menu form the burger icon-->
 <script>function dropFunction(){document.getElementById("dropMenu").classList.toggle("show");}</script>
-<div class="formTOP">
 
+<div class="formTOP">
+<!--Input box for deleting records-->
 <form action="" method="POST">
     <input type= "number" name="ID" placeholder="ID">
     <button type="submit">Delete</button>
@@ -31,6 +32,7 @@
 </div>
 
 <?php
+//Prints out table for record viewing.
 	include("..\CateringApp\connection.php");
     $sql = "SELECT * FROM cateringdata;";
     $result = mysqli_query( $connection, $sql );
@@ -41,10 +43,11 @@
 	    while($all =mysqli_fetch_assoc($result) ){
 	        echo "<tr><td>" .$all['ID'] . "</td><td>" .  $all['Date'] . "</td><td>" . $all['StartTime'] . "</td><td>". $all['EndTime'] . "</td><td>". $all['Room'] . "</td><td>". $all['DeliveryTime'] . "</td><td>". $all['MorningBreak'] . "</td><td>" .$all['AfternoonBreak'] . "</td><td>".$all['Floor'] . "</td><td>".$all['Attendees'] . "</td><td>".$all['Purpose'] . "</td><td>".$all['Restrictions'] . "</td><td>".$all['HotCold'] . "</td><td>".$all['Drinks'] . "</td><td>". $all['Vendor'] . "</td><td>". $all['Food'] . "</td></tr>"; 
 }
-echo "</table>"; //Close the table in HTML
+echo "</table>"; 
 	}
+    //Gets the id for the record the admin wishes to delete.
 	$deleteID=$_POST['ID']??'';
-	$dbDelete = "DELETE FROM `cateringdata` WHERE `cateringdata`.`ID` = $deleteID";//` quotations are needed for SQL
+	$dbDelete = "DELETE FROM `cateringdata` WHERE `cateringdata`.`ID` = $deleteID";//DO NOT CHANGE QUOTATION MARKS. REMOVAL OF `` FOR '' RESULTS IN ERROR.
 	mysqli_query( $connection, $dbDelete); 
 ?>
 </body>
