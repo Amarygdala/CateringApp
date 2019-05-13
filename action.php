85e2e7131f5f13d9a5ba72b65a1d38208d01673a
Amarygdala <49155656+Amarygdala@users.noreply.github.com>
2019-05-13 10:37:48 -0400

    Beautified

--- action.php
+++ action.php
@@ -1,4 +1,6 @@
 <?php
+//Vulnerable to SQL injection attacks (The login system isn't).
+//Gets the information from the form, then runs the query to insert into database.
 	include("..\CateringApp\connection.php");
 	$date = $_POST["date"]??'';
 	$start_time = $_POST["start_time"]??'';
@@ -17,4 +19,5 @@
 	$food = $_POST["food"]??'';
 	$dbinsert = "INSERT INTO cateringdata (Date,StartTime,EndTime,Room,DeliveryTime,MorningBreak,AfternoonBreak,Floor,Attendees,Purpose,Restrictions,HotCold,Drinks,Vendor,Food) VALUES ('$date', '$start_time', '$end_time', '$room', '$delivery_time','$morning_break', '$afternoon_break', '$floor', '$attendees', '$purpose','$restrictions','$hot_cold', '$drinks', '$vendor', '$food');";
 	mysqli_query( $connection, $dbinsert ); 
+
 	header("Location: ../CateringApp/form.php?=success");
