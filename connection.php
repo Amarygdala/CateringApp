79397c6dd3fd4c909200bbeb3d573b46f9b2068b
Amarygdala <49155656+Amarygdala@users.noreply.github.com>
2019-05-13 10:48:45 -0400

    Added short description for the usage of this file

--- connection.php
+++ connection.php
@@ -1,4 +1,5 @@
 <?php
+//Stores the $connection variable for use in other docs.
 $dbUsername = "root";
 $dbPassword = "";
 $connection = mysqli_connect("localhost", $dbUsername, $dbPassword,"cateringapp");
