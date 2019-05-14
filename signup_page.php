559eab4d67fb211c9d68c7000b98b75d7d111ae5
Amarygdala <49155656+Amarygdala@users.noreply.github.com>
2019-05-14 15:43:09 -0400

    Comment on the functionality of this code
    Create new users. Moves admin user back to admin page.

--- signup_page.php
+++ signup_page.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="loginStyle.css">
 
 	<div class="wrapper">
+	<!--For admin user to create new normal user-->
 	<h1>Signup</h1>
 	<form action="../CateringApp/signupCon.php" method="post">
 		<input type="text" name="uid" placeholder="Username">
@@ -13,7 +14,7 @@
 		<input type="password" name="pwd-repeat" placeholder="Repeat Password">
 		<button type="submit" name="signup-submit">Signup</button>
 	</form>
-
+	<!--Moves admin user back to admin page-->
 	<form action="../CateringApp/admin.php">
 		<button type="submit">Back</button>
 	</form>
