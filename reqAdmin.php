89340100ed3ae940cb74c606f4b6adfa5744708d
Amarygdala <49155656+Amarygdala@users.noreply.github.com>
2019-05-14 15:41:05 -0400

    Added comment on how this file is used

--- reqAdmin.php
+++ reqAdmin.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+//Used by being required in other files. Redirects to login page if user is not dshop. Redirects to login page after 10 minutes.
+	session_start();
+	header("refresh: 601"); 
     if (!isset($_SESSION['userUid'])||$_SESSION['userUid']!=="dshop") {
         header('Location: ../CateringApp/form.php?error=noaccess');
         exit();
