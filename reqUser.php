e33b203bf4fedac4f9e3bd0711f8a2b2dc57a058
Amarygdala <49155656+Amarygdala@users.noreply.github.com>
2019-05-14 15:41:24 -0400

    Added comment on how this file is used

--- reqUser.php
+++ reqUser.php
@@ -1,4 +1,5 @@
 <?php
+//Used by being required in other files. Redirects to login page if there is no user. Redirects to login page after 10 minutes.
 	session_start();
 	header("refresh: 601"); 
 	if(!isset($_SESSION['userId']))
