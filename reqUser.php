8db48ca64ba4d24742fc4963afbb9dd92c3b7ecb
Amarygdala <49155656+Amarygdala@users.noreply.github.com>
2019-05-17 09:41:20 -0400

    Update: made session timeout an error

--- reqUser.php
+++ reqUser.php
@@ -1,7 +1,7 @@
 <?php
 //Used by being required in other files. Redirects to login page if there is no user. Redirects to login page after 10 minutes.
 	session_start();
-	header("refresh: 601"); 
+	header("refresh: 605"); 
 	if(!isset($_SESSION['userId']))
 	{
 	    header('Location: ../login_page.php');
@@ -10,7 +10,7 @@
     if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 600)) {
 	    session_unset();
 	    session_destroy();
-	    header("Location:../login_page.php?sessiontimeout");
+	    header("Location:../login_page.php?error=sessiontimeout");
 }
 $_SESSION['LAST_ACTIVITY'] = time();
 ?>
