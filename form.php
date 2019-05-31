1e41b65c1ba8ad3941572854f888d8fda6c29e89
Xiang Weng <49155656+Amarygdala@users.noreply.github.com>
2019-05-31 08:56:38 -0400

    Update:Cost center+Organizer+Reordered input
    Added cost center, organizer fields, and reordered the inputs to match the display.

--- form.php
+++ form.php
@@ -1,4 +1,4 @@
-CATERING<?php
+<?php
     require "reqUser.php";
 ?>
 <!DOCTYPE html>
@@ -44,7 +44,7 @@ CATERING<?php
     <div id="dropMenu" class="menuContent">
         <a href="form.php">Form</a>
         <a href="showTable.php">Show Table</a>
-        <a href="deleteRecord.php">Delete Record</a>
+        <a href="deleteRecord.php">Delete/Change</a>
     </div>
 </div>
 <div class="burger"></div>
@@ -55,44 +55,52 @@ CATERING<?php
 <form action="action.php" method="POST">
    	<h1>SAP Catering Form</h1> 
 <div class="formclass">
+    <label for="purpose">Purpose of Meeting:</label>
+        <select name="purpose">
+            <option value="External">External</option>
+            <option value="Internal">Internal</option>
+        </select>
     <label for="date">Date:</label>
-        <input type= "date" name="date" required="required">
+        <input type= "date" name="date" class="formRight" required="required"></br>
+    <label for="floor">Floor:</label>
+        <select name="floor">
+            <option value="18">18</option>
+            <option value="19">19</option>
+            <option value="20">20</option>
+        </select>
+    <label for="room">Room:</label>
+        <input type= "text" name="room" class="formRight" required="required"></br>
+    <label for="attendees">Number of Attendees:</label>
+        <input type= "number" name="attendees" required="required">
+    <label for="food">Food:</label>
+       <input type="text" name="food" class="formRight"></br>
     <label for="delivery_time">Catering Delivery Time:</label>
-        <input type="time" name="delivery_time" required="required"></br>
+        <input type="time" name="delivery_time" required="required">
+    <label for="meal">Meal:</label>
+    	<select name="meal" class="formRight">
+    		<option value="Breakfast">Breakfast</option>
+    		<option value="Lunch">Lunch</option>
+    		<option value="Dinner">Dinner</option>
+    	</select></br>
     <label for="start_time">Time(Start):</label>
         <input type= "time" name="start_time" required="required">
     <label for="end_time">Time(End):</label>
-        <input type= "time" name="end_time" required="required"></br>
-    <label for="room">Room:</label>
-        <input type= "text" name="room" required="required">
+        <input type= "time" name="end_time" class="formRight" required="required"></br>
     <label for="morning_break" >Morning Break:</label>
-        <input type= "time" name="morning_break"></br>
+        <input type= "time" name="morning_break">
     <label for="afternoon_break">Afternoon Break:</label>
-        <input type= "time" name="afternoon_break">
-    <label for="floor">Floor:</label>
-        <select name="floor">
-    		<option value="18">18</option>
-    		<option value="19">19</option>
-    		<option value="20">20</option>
-    	</select></br>
-    <label for="attendees">Number of Attendees:</label>
-        <input type= "number" name="attendees" required="required">
-    <label for="purpose">Purpose of Meeting:</label>
-        <select name="purpose">
-        	<option value="External">External</option>
-        	<option value="Internal">Internal</option>
-		</select></br>
-	<label for="restrictions">Restrictions/Notes:</label>
+        <input type= "time" name="afternoon_break" class="formRight"></br>
+	<label for="restrictions">Notes/Dietary Restrictions:</label>
 	   <input type="text" name="restrictions">
-    <label for="hot_cold">Cold/Hot:</label>
-        <select name="hot_cold">
+    <label for="hot_cold">Hot/Cold:</label>
+        <select name="hot_cold" class="formRight">
         	<option value="Cold">Cold</option>
         	<option value="Hot">Hot</option>
 		</select></br>
 	<label for="drinks">Drinks(Coffee,Water,Tea,Juice,Pop):</label>
 	   <input type="text" name="drinks">
     <label for="vendor">Vendor:</label>
-        <input type="text" list="vendors" placeholder="Other">
+        <input type="text" list="vendors" class="formRight" required="required" placeholder="Other">
         <datalist name="vendor" id="vendors">
 			<option value="Aroma">Aroma</option>
 			<option value="Burger King">Burger King</option>
@@ -108,13 +116,22 @@ CATERING<?php
 			<option value="Soup Nutsy">Soup Nutsy</option>
 			<option value="Tim Hortons">Tim Hortons</option>
 		</datalist></br>
-	<label for="food">Food:</label>
-	   <input type="text" name="food">
-    <button type="submit">Submit</button>
+	<label for="cost_center">Cost Center(4 Digit #):</label>
+	 	<input type="number" min="1000" max="9999"  name="cost_center" required="required">
+	<label for="organizer">Organizer:</label>
+		<input type="text" name="organizer" class="formRight" required="required"></br>
+    <button type="submit" id="submitbutton">Submit</button>
 </form>
 </div>
 
+<!--<script type="text/javascript">
+	var insertpriv = <?php echo $_SESSION['insert']?>;
+	console.log('insertpriv1');
+	if(insertpriv!==1){
+		document.getElementById("submitbutton").disabled=true;
+	}
 
+</script>-->
 
 <img src="sap.png" class="sapimage">
 </body>
